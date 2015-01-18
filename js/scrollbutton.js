739ebf0b634fb6184eb61f40c3ae0cedd383bfc5
Margaret Yu <myu.margaret@gmail.com>
2015-01-17 21:57:18 -0800

    added enter site button, changed logo, added javascript files

---
+++ js/scrollbutton.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+	if ($(this).scrollTop() >= 50) {
+		$('#returntotop').fadeIn(300);
+	} else {
+		$('#returntotop').fadeOut(300); }
+});
+$('#returntotop').click(function() {
+  $('body,html').animate({
+    scrollTop : 0
+  }, 2000); 
+});
