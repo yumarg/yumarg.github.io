43050c4e86f8d36bf853dd6ee25810438de9667a
Margaret Yu <yumarg@users.noreply.github.com>
2021-02-11 21:29:06 -0800

    Fix link

--- seatMe/js/main.js
+++ seatMe/js/main.js
@@ -13,7 +13,7 @@ $(function() {
 	 }; 
 
 	$.ajax({
-		url: "http://margaretyu.me/seatMe/kimjeremywedding.csv",
+		url: "http://yumarg.github.io/seatMe/kimjeremywedding.csv",
 		dataType: "text",
 		success: function(content) {controller.readAndProcessCSV(content);},
 		error: function() {console.log("couldn't load csv");}
